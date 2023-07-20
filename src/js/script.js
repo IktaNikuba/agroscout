79aa38e51b646222ed6488c1c71479d09393241d
IktaNikuba <baukinv4@gmail.com>
2023-07-20 11:00:23 +0300

    Изменен конфиг Vite, package.json. Пофикшен баг с нераспазнаванием js. SVG slider не получилось сделать аддаптивным. Возвращаемся к идее бэкграунда для Hero

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,19 @@
-/* Открытие и закрытие навигационного меню */
-
 const menuOpenBtn = document.getElementById("open-menu");
 const menuCloseBtn = document.getElementById("close-menu");
 const menu = document.getElementById("navigation-menu");
 const body = document.querySelector(".page__body")
 
 menuOpenBtn.onclick = function() {
-    menuOpenBtn.classList.toggle("cut-down");
-    menuCloseBtn.classList.toggle("cut-down");
-    menu.classList.toggle("cut-down");
-    body.classList.toggle("body-overflow-hidden")
+	menuOpenBtn.classList.toggle("cut-down");
+	menuCloseBtn.classList.toggle("cut-down");
+	menu.classList.toggle("cut-down");
+	body.classList.toggle("body-overflow-hidden")
 };
 
 menuCloseBtn.onclick = function() {
-    menuOpenBtn.classList.toggle("cut-down");
-    menuCloseBtn.classList.toggle("cut-down");
-    menu.classList.toggle("cut-down");
-    body.classList.toggle("body-overflow-hidden")
+	menuOpenBtn.classList.toggle("cut-down");
+	menuCloseBtn.classList.toggle("cut-down");
+	menu.classList.toggle("cut-down");
+	body.classList.toggle("body-overflow-hidden")
 };
+
