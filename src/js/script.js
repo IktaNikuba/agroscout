4ab756509b0c240f445c2ae7e6beb1e2db0b5ffb
IktaNikuba <baukinv4@gmail.com>
2023-08-29 17:15:25 +0300

    Исправлния

--- src/js/script.js
+++ src/js/script.js
@@ -8,23 +8,25 @@ menuOpenBtn.onclick = () => {
 	menuOpenBtn.classList.toggle("cut-down");
 	menuCloseBtn.classList.toggle("cut-down");
 	menu.classList.toggle("cut-down");
-	body.classList.toggle("body-overflow-hidden");
+	body.classList.toggle("body-overflow-hidden--md");
 };
 
 menuCloseBtn.onclick = () => {
 	menuOpenBtn.classList.toggle("cut-down");
 	menuCloseBtn.classList.toggle("cut-down");
 	menu.classList.toggle("cut-down");
-	body.classList.toggle("body-overflow-hidden");
+	body.classList.toggle("body-overflow-hidden--md");
 };
 
 /* Миникорзина */
-const modalCart = document.querySelector('.minicart');
+const modalCart = document.querySelector('.mini-cart');
 
 document.getElementById("open-mini-cart").onclick = function() {
 	modalCart.showModal();
+	body.classList.toggle("body-overflow-hidden");
 };
 
 document.getElementById("close-mini-cart").onclick = function() {
 	modalCart.close();
+	body.classList.toggle("body-overflow-hidden");
 };
